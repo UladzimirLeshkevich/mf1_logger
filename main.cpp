6f0b1a14c681856b0213084581685cca1a7f52fd
Vladimir Leshkevich <v.leshkevich@tcp-soft.com>
2019-10-29 15:03:02 +0300

    some classes added

--- main.cpp
+++ main.cpp
@@ -1,25 +1,44 @@
 #include "log_manager.h"
 
+class A
+{
+  public:
+    A() : logger(LogManager::get_logger("A_system")) { logger << " A system log " << 77 << CRITICAL; }
+    std::shared_ptr<Log> logger;
+
+    void log()
+    {
+        logger << " A log() " << DEBUG;
+    }
+};
+
+class B
+{
+  public:
+    B() : logger(LogManager::get_logger("B_system")) { logger << " B system log " << 88 << CRITICAL; }
+    std::shared_ptr<Log> logger;
+};
+
 //============================================================
 int main()
 {
     std::shared_ptr<Log> logger;
 
-    logger = LogManager::get_logger("main");
+    logger = LogManager::get_logger("main_system");
 
-    logger->open_logfile("D:\\lll2.txt");
+    logger->open_logfile("D:\\log_s.txt");
 
-    logger << " Data from settings file '"
-           << "D:\\lll2.txt"
-           << "' :" << INFO;
+    A a;
 
-    logger << " ergwbtg " << 22 << INFO;
+    B b;
 
-    logger << 22 << " faewr " << INFO;
+    //a.logger << " A system log " << 77 << CRITICAL;// NOT OK !!
 
-    logger << INFO << 2;
+    //a.log(); // NOT OK !!
 
-    logger << " " << 323 << CRITICAL;
+    logger << " main system log " << 66 << INFO;
+
+    logger << 66 << " main system log " << WARNING;
 
     logger->close_log();
 
