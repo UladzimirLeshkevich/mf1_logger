e63848deedfc6a73a400c11648a0d7d40904d42f
Vladimir Leshkevich <v.leshkevich@tcp-soft.com>
2019-10-30 16:07:38 +0300

    OK

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ class A
     static const std::string sA;
     A() : logger(LogManager::get_logger(sA))
     {
-        //logger->open_logfile("D:\\lll2.txt");
         logger << " A constructor " << 77 << CRITICAL;
     }
     std::shared_ptr<Log> logger;
@@ -16,7 +15,6 @@ class A
     ~A()
     {
         std::cout << " ~A() " << std::endl;
-        //logger->close_log();
     }
 };
 
@@ -44,11 +42,11 @@ int main()
 
     a2.logger << " A a2 from main " << 434523 << INFO;
 
-    logger << 22 << " from main " << INFO;
+    logger << 22 << " from main DLL " << INFO;
 
     logger << " from main " << 323 << CRITICAL;
 
-    logger->close_log(); // open/close in main
+    logger->close_log();
 
     return 0;
 }
